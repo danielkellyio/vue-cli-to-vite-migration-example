0cb953ccf2d73d39ca811a4a084e10dddbafb438
Daniel Kelly <me@danielkelly.io>
2021-10-29 11:32:07 -0500

    migrate to vite

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ npm install
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+npm run dev
 ```
 
 ### Compiles and minifies for production
 ```
 npm run build
+```
+
+### Preview production build
+```
+npm run serve
 ```
 
 ### Lints and fixes files
--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  base: import.meta.env.BASE_URL,
   routes,
 });
 
